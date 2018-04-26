0f58b41c00b6702d826239128d1fd4d685b54cc6
Bartosz <37622841+Bartewicz@users.noreply.github.com>
2018-04-26 16:29:18 +0200

    Update Sidebar.js
    Edit stylistics and 'Herer' -> 'Here'

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Sidebar = (props) =>
-(
-<div>Herer will be sidebar</div>
-)
+const Sidebar = (props) => (
+  
+  <div>Here will be sidebar</div>
 
+)
 
 export default Sidebar
