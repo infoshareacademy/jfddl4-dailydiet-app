21e9cfb5fa39134fd9b28b81d4a9a34ab1254249
Szymon Oleszek <szymqwerty@gmail.com>
2018-04-27 17:02:29 +0200

    WIP

---
+++ src/Controls.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
+
+const Controls = (props) => (
+    <Container>
+        <TextField/> {/* here you can type name of product and search for looking products */}
+        
+
+    </Container>
+
+
+
+)
--- src/ProductsSearchList.js
+++ src/ProductsSearchList.js
@@ -2,6 +2,18 @@ import React from 'react'
 
 
 class ProductsSearchList extends React.Component {
+
+    state = {
+        products: [
+            { name: 'Potatoe', uid: Date.now() },
+            { name: 'Tomatoe', uid: Date.now() }
+        ],
+
+s
+    }
+
+
+
     render() {
         return (
             <div>ytrew</div>
