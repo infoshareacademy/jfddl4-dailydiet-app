3c0daa1ed6263ef8bae7845526819d00692714d8
madzia89 <wojcicka.ma@gmail.com>
2018-04-27 16:42:27 +0200

    add charts to work with

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Grid, Row, Col } from 'react-flexbox-grid'
+import PieChart from './Charts/PieChart'
+import Chart2 from './Charts/Chart2'
 
 
-const Dashboard = (props) => (
+const Dashboard = () => (
 
-    <div> Here will be dashboard </div>
+    <Grid fluid>
+        <Row>
+            <Col xs={6} md={3}>
+                <PieChart/>
+            </Col>
+            <Col xs={6} md={3}>
+                <Chart2/>
+            </Col>
+        </Row>
+    </Grid>
 
 )
 
