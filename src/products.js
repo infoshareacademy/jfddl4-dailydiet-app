83c69e5d3814e7fb56901c78ee0366c253066b06
madzia89 <wojcicka.ma@gmail.com>
2018-05-07 16:58:13 +0200

    add some products

---
+++ src/products.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+products = [
+    {
+        category: '',
+        name: '',
+        carbohydrates: '',
+        kcal: '',
+        fat: '',
+        id: 656464651
+    },
+    {
+        category: 'meat',
+        name: 'sausage',
+        carbohydrates: '306',
+        kcal: '341',
+        fat: '29',
+        id: 548641532
+    },
+    {
+        category: 'meat',
+        name: 'ham',
+        carbohydrates: '1,5',
+        kcal: '145',
+        fat: '6',
+        id: 656464651
+    },
+    {
+        category: 'meat',
+        name: 'pate',
+        carbohydrates: '1,5',
+        kcal: '319',
+        fat: '28',
+        id: 656464651
+    },
+
+]
