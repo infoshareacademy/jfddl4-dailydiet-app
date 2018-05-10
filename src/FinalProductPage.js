bebf5c328cf9f4a9a2cd591034ed8fc539382e04
madzia89 <wojcicka.ma@gmail.com>
2018-05-10 15:33:49 +0200

    add pagination, and pagination styles

--- src/FinalProductPage.js
+++ src/FinalProductPage.js
@@ -3,21 +3,22 @@ import {List, ListItem} from 'material-ui/List'
 import Avatar from 'material-ui/Avatar'
 import IconButton from 'material-ui/IconButton'
 import StarBorder from 'material-ui/svg-icons/toggle/star-border'
-import {Redirect} from 'react-router'
+import ReactPaginate from 'react-paginate';
 
+const ITEMS_PER_PAGE = 5
 
 class Products extends Component {
     state = {
         products: null,
+        numberOfPages: 0,
+        activePage: 0
     }
 
 
     componentDidMount() {
-
         fetch('https://dailydiet-app.firebaseio.com/products/.json')
             .then((response) => response.json())
             .then((myJson) => {
-
                 const dataInArray = (
                     Object.entries(myJson)
                         .map(el => ({
@@ -28,12 +29,20 @@ class Products extends Component {
 
                 )
 
-                this.setState({products: dataInArray})
+                this.setState({
+                    products: dataInArray,
+                    numberOfPages: Math.ceil(dataInArray.length / ITEMS_PER_PAGE)
+                })
                 console.log(dataInArray)
             })
     }
 
-    // redirect //fixme
+
+    handlePageClick = (e) => {
+        this.setState({
+            activePage: e.selected
+        })
+    }
 
 
     render() {
@@ -44,21 +53,42 @@ class Products extends Component {
                         'loading...'
                         :
                         <List>
-                            {this.state.products.map(
-                                aProduct => (
-                                    <ListItem key={aProduct.key}
-                                              primaryText={aProduct.value}
-                                              insetChildren={true}
-                                              leftAvatar={<Avatar src={aProduct.pic}/>}
-                                              rightAvatar={<IconButton><StarBorder color="lightBlue"/></IconButton>}
-                                              onClick={<Redirect to='https://facebook.com'/>}//fixme
-                                    >
-                                    </ListItem>
-                                )
-                            )
+                            {
+                                this.state.products
+                                    .filter((aProduct, index) => (
+                                        this.state.activePage * ITEMS_PER_PAGE <= index
+                                        &&
+                                        (this.state.activePage + 1) * ITEMS_PER_PAGE > index
+                                    ))
+                                    .map(
+                                        aProduct => (
+                                            <ListItem key={aProduct.key}
+                                                      primaryText={aProduct.value}
+                                                      insetChildren={true}
+                                                      leftAvatar={<Avatar src={aProduct.pic}/>}
+                                                      rightAvatar={<IconButton><StarBorder
+                                                          color="lightBlue"/></IconButton>}
+                                            >
+
+                                            </ListItem>
+                                        )
+                                    )
                             }
                         </List>
                 }
+                <ReactPaginate
+                    previousLabel={"previous"}
+                    nextLabel={"next"}
+                    breakLabel={<a href="">...</a>}
+                    breakClassName={"break-me"}
+                    pageCount={this.state.numberOfPages}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"activePage"}
+                />
             </div>
 
         )
