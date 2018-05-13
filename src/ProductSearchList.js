258f4b91214af6e6ae815ff67563436fa2dce497
Szymon Oleszek <szymqwerty@gmail.com>
2018-05-13 14:25:19 +0200

    calories slider works

--- src/ProductSearchList.js
+++ src/ProductSearchList.js
@@ -10,21 +10,13 @@ import Toggle from 'material-ui/Toggle';
 
 class ProductsSearchList extends React.Component {
 
-
-
-    handleSlider = (event, value) => {
-        this.setState({ slider: value });
-    };
-
-
-
-
     state = {
         lookingProduct: '', // część tekstu po którym jest wyszukiwany produkt
         productsList: [], // kompletna baza danych 
-        filtredListOfProduct: [],
-        slider: 500,
-        toggleButtonState: false
+        filtredListOfProductByTextField: [], // lista produktów przefiltrowana przez wyszukiwarkę po nazwie 
+        filtredListOfProductBySlider: [], // lista produktów przefiltrowana przez slider i wyszukiwarkę po nazwie  // LISTA WYŚWIETLENIA
+        calories: 500, //kalorie
+        toggleButtonState: false //przełącznik możliwiający włączanie/wyłączanie wyszukiwania po kaloriach
     }
 
 
@@ -42,72 +34,95 @@ class ProductsSearchList extends React.Component {
                 )
                 this.setState({ productsList: dataInArray })
                 console.log(dataInArray)
-            })
+            }).then(() => { // zapis całej listy produktów do przefiltrowanej listy po to żeby po załadowaniu strony gdy textField jest pusty widoczne były wszystkie produkty
+                if (this.state.lookingProduct === '') this.setState({ filtredListOfProductByTextField: this.state.productsList })
+            }
+            )
+
+
     }
 
 
+    handleSlider = (event, value) => {
+
+        this.setState({ calories: value });
+        this.searchProducts()
+    }
 
 
     searchProducts = () => {
 
-        this.setState({
-            filtredListOfProduct: this.state.productsList.filter((el, i, arr) => {
-
+        // filtrowanie produktów po wpisywaniu nazwy do textFieldu
+        // jeżeli nic nie jest wpisane wyświetl wszystkie
+        if (this.state.lookingProduct == '') this.setState({ filtredListOfProductByTextField: this.state.productsList })
+        else this.setState({
+            filtredListOfProductByTextField: this.state.productsList.filter((el, i, arr) => {
                 if (el.value.name.indexOf(this.state.lookingProduct) != -1) return true
             })
         })
+
+
+        //filtrowanie produktów za pomocą slidera (kalorie)
+
+        if (this.state.toggleButtonState) {
+
+            this.setState({
+
+                filtredListOfProductBySlider: this.state.filtredListOfProductByTextField.filter((el, i, arr) => {
+                    if (el.value.kcal < this.state.calories) return true
+                })
+
+            })
+
+        }
     }
-
-
     render() {
         return (
             <Container>
-
                 <TextField
                     hintText={'Type name of looking product'}
                     fullWidth={true}
-                    onChange={(target, newValue) => this.setState({ lookingProduct: newValue })} // w onChange-u filter 
+                    onChange={(target, newValue) => {
+                        this.setState({ lookingProduct: newValue })
+                        this.searchProducts()
+                    }}// w onChange-u filter 
 
                 />
-
-                <RaisedButton
-                    label="Search"
-                    primary={true}
-                    onClick={() => this.searchProducts()}
-                />
-
-                {
-                    this.state.filtredListOfProduct.map((el) => (
-
-                        <div key={el.key}> {el.value.name} </div>
-
-                    ))
-
-
-                }
-
                 <Container>
-                    <div>
-                        <Toggle
-                            label="Disable searching by calories"
-                            onToggle={(e,i)=>this.setState({toggleButtonState: i})}
-                            toggled={this.state.toggleButtonState}
-                        />
-                        <Slider
-                            min={0}
-                            max={1000}
-                            step={1}
-                            value={this.state.slider}
-                            onChange={this.handleSlider}
-                            onClick={() => this.setState({toggleButtonState: true})}
-                        />
-                        <p>
-                            <span>{'Value of calories: '}</span>
-                            <span>{this.state.slider}</span>
-                        </p>
-                    </div>
+
+                    <Toggle
+                        label="Disable searching by calories"
+                        onToggle={(e, i) => this.setState({ toggleButtonState: i })}
+                        toggled={this.state.toggleButtonState}
+                    />
+                    <Slider
+                        min={0}
+                        max={500}
+                        step={1}
+                        value={this.state.calories}
+                        onChange={
+                            (event, value) =>
+                                this.handleSlider(event, value)
+
+                        }
+                        onClick={() => {
+                            this.setState({ toggleButtonState: true })
+                        }}
+                    />
+                    <p>
+                        <span>{'Value of calories: '}</span>
+                        <span>{this.state.calories}</span>
+                    </p>
+
 
                 </Container>
+
+                {
+                    this.state.filtredListOfProductBySlider.map((el, i) => (
+                        <div key={el.key}>{i} {el.value.name} </div>
+                    ))
+                }
+
             </Container>
         )
     }
