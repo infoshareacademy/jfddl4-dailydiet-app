1086f2fbccc1cdeaebf52a7c8747999373dfa78c
madzia89 <wojcicka.ma@gmail.com>
2018-05-26 10:27:25 +0200

    better solution for assigning actionValues to state

--- src/Components/MealPlan.js
+++ src/Components/MealPlan.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import {connect} from "react-redux";
-import {mealDate, mealSyncer, getLunch, getBreakfast, getDinner} from '../state/mealPlan'
+import {mealDate, mealSyncer} from '../state/mealPlan'
 import DatePicker from 'material-ui/DatePicker';
 import RaisedButton from 'material-ui/RaisedButton'
-import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
-import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 
 
 class MealPlan extends React.Component {
@@ -32,6 +29,7 @@ class MealPlan extends React.Component {
                     onChange={(ev, value) => {
                         this.props.mealDate(value)
                         this.handleOpen()
+                        this.props.mealSyncer()
                     }}
                 >
                     <RaisedButton
@@ -39,10 +37,6 @@ class MealPlan extends React.Component {
                         backgroundColor={'#E65100'}
                         label={<span style={{color: 'white'}}>Show meal plan</span>}
                         onClick={() => {
-                            this.props.mealSyncer()
-                            this.props.getBreakfast()
-                            this.props.getLunch()
-                            this.props.getDinner()
                             this.handleClose()
                         }
                         }
@@ -59,9 +53,6 @@ const mapStateToProps = state => ({})
 const mapDispatchToProps = dispatch => ({
     mealDate: (theValue) => dispatch(mealDate(theValue)),
     mealSyncer: () => dispatch(mealSyncer()),
-    getBreakfast: () => dispatch(getBreakfast()),
-    getLunch: () => dispatch(getLunch()),
-    getDinner: () => dispatch(getDinner()),
 })
 
 export default connect(
--- src/state/mealPlan.js
+++ src/state/mealPlan.js
@@ -3,9 +3,6 @@ import moment from "moment/moment";
 
 const MEAL_DATE = 'mealPlan/MEAL_DATE'
 const MEAL_PLAN_TO_GET_FROM_DB = 'mealPlan/MEAL_PLAN_TO_GET_FROM_DB'
-const MEAL_PLAN_BREAKFAST = 'mealPlan/MEAL_PLAN_BREAKFAST'
-const MEAL_PLAN_LUNCH = 'mealPlan/MEAL_PLAN_LUNCH'
-const MEAL_PLAN_DINNER = 'mealPlan/MEAL_PLAN_DINNER'
 
 
 export const mealSyncer = () => (dispatch, getState) => {
@@ -23,61 +20,10 @@ export const mealDate = (dateOfMeal) => ({
     dateOfMeal
 })
 
-
 export const mealPlanToGetFromDB = (acceptToSeeMealPlan) => ({
     type: MEAL_PLAN_TO_GET_FROM_DB,
     acceptToSeeMealPlan
-
 })
-
-
-export const getBreakfast = () => (dispatch, getState) => {
-    const userUid = getState().auth.user.uid
-    const mealDate = getState().mealPlan.mealDate
-    return db.ref(`/users/${userUid}/meals/${mealDate}/breakfast`).on(
-        'value',
-        (snapshot) => dispatch(
-            mealPlanbreakfast(snapshot.val() || [])
-        ))
-}
-
-export const mealPlanbreakfast = (breakfastValue) => ({
-    type: MEAL_PLAN_BREAKFAST,
-    breakfastValue
-
-})
-
-
-export const getLunch = () => (dispatch, getState) => {
-    const userUid = getState().auth.user.uid
-    const mealDate = getState().mealPlan.mealDate
-    return db.ref(`/users/${userUid}/meals/${mealDate}/lunch`).on(
-        'value',
-        (snapshot) => dispatch(
-            mealPlanLunch(snapshot.val() || [])
-        ))
-}
-export const mealPlanLunch = (lunchValue) => ({
-    type: MEAL_PLAN_LUNCH,
-    lunchValue
-
-})
-
-export const getDinner = () => (dispatch, getState) => {
-    const userUid = getState().auth.user.uid
-    const mealDate = getState().mealPlan.mealDate
-    return db.ref(`/users/${userUid}/meals/${mealDate}/dinner`).on(
-        'value',
-        (snapshot) => dispatch(
-            mealPlanDinner(snapshot.val() || [])
-        ))
-}
-export const mealPlanDinner = (dinnerValue) => ({
-    type: MEAL_PLAN_DINNER,
-    dinnerValue
-
-})
-
 
 const initialState = {}
 
@@ -91,24 +37,16 @@ export default (state = initialState, action) => {
             }
 
         case MEAL_PLAN_TO_GET_FROM_DB :
+            const meals = action.acceptToSeeMealPlan
+            const breakfast = meals.breakfast
+            const lunch = meals.lunch
+            const dinner = meals.dinner
             return {
                 ...state,
-                meals: action.acceptToSeeMealPlan
-            }
-        case MEAL_PLAN_BREAKFAST :
-            return {
-                ...state,
-                breakfast: action.breakfastValue
-            }
-        case MEAL_PLAN_LUNCH :
-            return {
-                ...state,
-                lunch: action.lunchValue
-            }
-        case MEAL_PLAN_DINNER :
-            return {
-                ...state,
-                dinner: action.dinnerValue
+                meals,
+                breakfast,
+                lunch,
+                dinner
             }
 
         default:
